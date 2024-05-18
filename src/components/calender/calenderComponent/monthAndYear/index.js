540563465202eefa290ed0155ed0ae0ff88e22bd
jafar <mjafer657@gmail.com>
2024-05-18 14:25:47 +0530

    completed year update and its css

--- src/components/calender/calenderComponent/monthAndYear/index.js
+++ src/components/calender/calenderComponent/monthAndYear/index.js
@@ -8,22 +8,43 @@ function YearAndMonth({ currMonth, year, months, setMonth, setYear }) {
         const index = months.indexOf(selected);
         setMonth(index);
     }
+
+    const handleYearChange = (event) => {
+        setYear(event.target.value)
+    }
+
+    const yearOptions = [];
+
+    for (let i = 1900; i < 2200; i++) {
+        yearOptions.push(
+            <option value={i} key={i}>{i}</option>
+        )
+    }
     return (
         <>
-            {/* <h2>{months[month].toUpperCase()}</h2> */}
-            {/* <p>{months}</p> */}
             <div className='monthContainer'>
                 <select value={months[currMonth]} onChange={handleMonthChange} className='selectMonth'>
                     <optgroup>
                         {months.map((month) => {
                             return <option value={month} key={month}>{
-                                <h2 className='currMonth'>{month}</h2>
+                                <div>{ month }</div>
                             }</option>
                         })}
                     </optgroup>
                 </select>
             </div>
-            <h2>{year}</h2>
+
+            <div className='yearContainer'>
+                <select value={year} onChange={handleYearChange} className='selectYear'>
+                    <optgroup>
+                        {
+                            yearOptions
+                        }
+                    </optgroup>
+
+                </select>
+            </div>
+            {/* <h2>{year}</h2> */}
         </>
     )
 }
